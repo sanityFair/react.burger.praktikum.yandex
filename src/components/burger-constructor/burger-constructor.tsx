ba0a2be248433ac8e0f26b92cb13cf3176c0269b
sanityFair <106737730+sanityFair@users.noreply.github.com>
2024-09-20 16:49:15 +0300

    feat(*): add order info

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,6 +1,8 @@
 import { MOCK } from "@/constants";
 import {
+  Button,
   ConstructorElement,
+  CurrencyIcon,
   DragIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
@@ -8,30 +10,40 @@ import burgerConstructorStyles from "./burger-constructor.module.css";
 
 export const BurgerConstructor = () => {
   return (
-    <section className={burgerConstructorStyles.root.concat(" mt-20")}>
-      {MOCK.map((item, index) => (
-        <div className={burgerConstructorStyles.item}>
-          {(index !== 0 && index !== MOCK.length - 1) && (
-            <DragIcon type="primary" />
-          )}
-          <ConstructorElement
-            type={
-              index === 0
-                ? "top"
-                : index === MOCK.length - 1
-                ? "bottom"
-                : undefined
-            }
-            isLocked={
-              index === 0 ? true : index === MOCK.length - 1 ? true : false
-            }
-            text={item.name}
-            price={item.price}
-            thumbnail={item.image}
-            key={item._id}
-          />
-        </div>
-      ))}
+    <section className="mt-20">
+      <div className={burgerConstructorStyles.root.concat(" mb-10")}>
+        {MOCK.map((item, index) => (
+          <div className={burgerConstructorStyles.item}>
+            {index !== 0 && index !== MOCK.length - 1 && (
+              <DragIcon type="primary" />
+            )}
+            <ConstructorElement
+              type={
+                index === 0
+                  ? "top"
+                  : index === MOCK.length - 1
+                  ? "bottom"
+                  : undefined
+              }
+              isLocked={
+                index === 0 ? true : index === MOCK.length - 1 ? true : false
+              }
+              text={item.name}
+              price={item.price}
+              thumbnail={item.image}
+              key={item._id}
+            />
+          </div>
+        ))}
+      </div>
+      <div className={burgerConstructorStyles.order}>
+        <span className="text text_type_digits-medium ">
+          624 <CurrencyIcon type="primary" />
+        </span>
+        <Button htmlType="button" type="primary" size="large" extraClass="ml-4">
+          Оформить заказ
+        </Button>
+      </div>
     </section>
   );
 };
